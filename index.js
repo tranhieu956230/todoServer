d7ecedfd79efd3e336c6def55271b6a52512ba43
Tran Hieu <tranhieu956230@gmail.com>
2018-05-18 17:59:31 +0700

    v2

--- index.js
+++ index.js
@@ -2,14 +2,29 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routes/api');
 const cors = require('cors');
+const socket = require('socket.io');
 var app = express();
 const mongoose = require('mongoose');
+
+//
 mongoose.connect('mongodb://hieu:hieu@ds149221.mlab.com:49221/tododb');
+
 app.use(cors());
 app.use(bodyParser.json());
 app.use(routes);
+//error handling middleware
+app.use(function(err,req,res,next){
+      res.status(404).send({status:false,error: err.message})
+})
 
+const server = app.listen(process.env.PORT || 4000,function(){
+    console.log("listening for request on port 4000");
+})
 
-app.listen(process.env.PORT || 4000,function(){
-    console.log("now listening to port 4000");
+const io = socket(server);
+
+io.on('connection',function(client){
+     client.on('change',() => {
+        io.sockets.emit('handleChange');
+     })
 })
--- routes/api.js
+++ routes/api.js
@@ -3,37 +3,37 @@ const router = express.Router();
 const todoModel = require('../models/todo');
 
 
-router.post('/todo', function (req, res) {
+router.post('/todo', function (req, res, next) {
     todoModel.create(req.body).then(function (response) {
-        res.send(response);
-    })
+        res.send({ status: "success", data: todo });
+    }).catch(next);
 })
 
-
-router.get('/todo', function (req, res) {
-    todoModel.find(function (err, todos) {
-        res.send(todos)
-    })
+router.get('/todo', function (req, res, next) {
+    todoModel.find().then(todos => {
+        res.send({ status: "success", data: todos });
+    }).catch(next);
 })
 
-router.delete('/todo/:_id', function (req, res) {
-    todoModel.findOneAndRemove({ _id: req.params._id }, function (err, todo) {
-        res.send(todo);
-    })
+router.delete('/todo/:_id', function (req, res, next) {
+    todoModel.findOneAndDelete({ _id: req.params._id }).then(todo => {
+        res.send({ status: "success", data: todo });
+    }).catch(next);
 })
 
-router.put('/todo/:_id', function (req, res) {
-    todoModel.findOneAndUpdate({ _id: req.params._id }, { todo: req.body.todo }, function (err, todo) {
-        res.send(todo);
-    })
+router.put('/todo/:_id', function (req, res, next) {
+    todoModel.findOneAndUpdate({ _id: req.params._id }, { todo: req.body.todo }, { new: true }).then(todo => {
+        res.send({ status: "success", data: todo });
+    }).catch(next);
 })
 
-router.put('/todo/:_id/toggle', function (req, res) {
-    todoModel.findById({ _id: req.params._id }, function (err, todo) {
-        todoModel.findOneAndUpdate({ _id: req.params._id }, { completed: !todo.completed }, function (req, obj) {
-            res.send(obj);
+router.put('/todo/:_id/toggle', function (req, res, next) {
+    todoModel.findById({ _id: req.params._id }).then(todo => {
+        todoModel.findOneAndUpdate({ _id: req.params._id }, { completed: !todo.completed }, { new: true }).then(() => {
+            res.send({ status: "success", data: todo });
         })
-    })
+    }).catch(next);
 })
+
 
 module.exports = router;
